093705884a41e6472397f20f614264790ec9d930
Yunior C. Fonseca Reyna <fonsecareyna82@gmail.com>
2020-07-10 19:22:32 +0200

    buildbot automated-update

---
+++ html/search/all_d.js
@@ -0,0 +1,22 @@
+var searchData=
+[
+  ['n',['n',['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#ac46fa68291e1e47551ca7f9b2023dd82',1,'xmipp::viewer::models::ImageGalleryTableModel']]],
+  ['name',['name',['../classxmipp_1_1jni_1_1Classifier_1_1Parameter.html#a99f9ba18bf0c1d599d75f4899e0b87e7',1,'xmipp::jni::Classifier::Parameter']]],
+  ['needstraining',['needsTraining',['../classxmipp_1_1jni_1_1Classifier.html#af09b6d7192dbd459357d76a33e2cf3f6',1,'xmipp.jni.Classifier.needsTraining()'],['../classxmipp_1_1jni_1_1PickingClassifier.html#a5dfa1a47f796e5cfeeb73ad72a61d186',1,'xmipp.jni.PickingClassifier.needsTraining()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1training_1_1model_1_1GenericClassifier.html#a139375a43f7c83afb9e1595585154d66',1,'xmipp.viewer.particlepicker.training.model.GenericClassifier.needsTraining()']]],
+  ['newline',['newline',['../classxmipp_1_1viewer_1_1ViewerTest.html#a707268838e7fc39ea0fc9d8943e65dd2',1,'xmipp::viewer::ViewerTest']]],
+  ['next_5fmicrograph_5fkey',['NEXT_MICROGRAPH_KEY',['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePickerJFrame.html#a49e0dc1eebcfa0174fac8b7d2fe0f10e',1,'xmipp::viewer::particlepicker::ParticlePickerJFrame']]],
+  ['nextcolor',['nextcolor',['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePicker.html#af42e955e3f05cb2028ad9e038b9a5ed0',1,'xmipp::viewer::particlepicker::ParticlePicker']]],
+  ['no_5fgeo',['NO_GEO',['../classxmipp_1_1utils_1_1Params.html#ac5d196e11ce8d43ad51dcd89c1002e85',1,'xmipp::utils::Params']]],
+  ['no_5foption',['NO_OPTION',['../classxmipp_1_1utils_1_1XmippQuestionDialog.html#a06bd62183a77db22f44ebfdca7b30b05',1,'xmipp::utils::XmippQuestionDialog']]],
+  ['no_5fwrap',['NO_WRAP',['../classxmipp_1_1utils_1_1Params.html#a91f0b8a047659b16e198847597da7c22',1,'xmipp::utils::Params']]],
+  ['none',['None',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#adb05937c5f3cf9021f520e6565aef179',1,'xmipp::viewer::particlepicker::Format']]],
+  ['normalize',['NORMALIZE',['../classxmipp_1_1utils_1_1XmippResource.html#ab233aadb9f2fbbb5c78d0966d3f5f86a',1,'xmipp.utils.XmippResource.NORMALIZE()'],['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#aaff95c63f90e40971050a680f72490c5',1,'xmipp.ij.commons.ImagePlusReader.normalize()'],['../classxmipp_1_1viewer_1_1models_1_1GalleryData.html#ac302d722cca2d8082bcefd382711ce00',1,'xmipp.viewer.models.GalleryData.normalize()']]],
+  ['normalize_5fcalculated',['normalize_calculated',['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#a352e7ef10b5557b8d9646d3ccf6ae6f9',1,'xmipp::viewer::models::ImageGalleryTableModel']]],
+  ['normalize_5fmax',['normalize_max',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#a798c9a5af25307728dbacbae2f34767b',1,'xmipp::ij::commons::ImagePlusReader']]],
+  ['normalize_5fmin',['normalize_min',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#acadfe9a7f88439164a28a509a8c77c96',1,'xmipp.ij.commons.ImagePlusReader.normalize_min()'],['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#a54c15293fa69f680793d4ce3eadd8aa4',1,'xmipp.viewer.models.ImageGalleryTableModel.normalize_min()']]],
+  ['normalizeimageplus',['normalizeImagePlus',['../classxmipp_1_1ij_1_1commons_1_1XmippImageConverter.html#abca6fda658cf95134f39c607f5cfa879',1,'xmipp::ij::commons::XmippImageConverter']]],
+  ['numberofclasses',['numberOfClasses',['../classxmipp_1_1viewer_1_1models_1_1ClassInfo.html#af61b336adcddc7c8324962b0a7924de3',1,'xmipp::viewer::models::ClassInfo']]],
+  ['numberofimages',['numberOfImages',['../classxmipp_1_1viewer_1_1models_1_1ClassInfo.html#a08cebe814ac9e9e13187f7e1e11f7f36',1,'xmipp::viewer::models::ClassInfo']]],
+  ['numbertocolor',['numberToColor',['../classxmipp_1_1viewer_1_1particlepicker_1_1ColorHelper.html#aea9d5f253463b9547fd02af66b561ee3',1,'xmipp::viewer::particlepicker::ColorHelper']]],
+  ['numbertocolorpercentage',['numberToColorPercentage',['../classxmipp_1_1viewer_1_1particlepicker_1_1ColorHelper.html#a5d6135a4f2af90a2ee602773cfcbb38e',1,'xmipp::viewer::particlepicker::ColorHelper']]]
+];
