78457df6cf3c385fb64922f5147b371a9b762d61
biocompwebs <biocompwebs@gmail.com>
2020-07-28 09:46:23 +0200

    buildbot automated-update

--- html/search/variables_18.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['y',['y',['../classxmipp_1_1jni_1_1Particle.html#aa04f5897bafdf4f40889f189e10e6e33',1,'xmipp::jni::Particle']]],
-  ['y_5faxis',['Y_AXIS',['../enumxmipp_1_1utils_1_1CompoundIcon_1_1Axis.html#a5f1dd9e943b9c6c8e4735eb05804aa9d',1,'xmipp::utils::CompoundIcon::Axis']]],
-  ['y_5fneg',['Y_NEG',['../classxmipp_1_1jni_1_1ImageGeneric.html#aef3d3fe751ce3a2aa44dadeb01fe0fee',1,'xmipp::jni::ImageGeneric']]],
-  ['y_5fpos',['Y_POS',['../classxmipp_1_1jni_1_1ImageGeneric.html#ad1dba3a6cf3ebdf53b8aa33d5a5a6c49',1,'xmipp::jni::ImageGeneric']]],
-  ['yes_5foption',['YES_OPTION',['../classxmipp_1_1utils_1_1XmippQuestionDialog.html#a34f24ab345ac8a70e6366cc6120fc4eb',1,'xmipp::utils::XmippQuestionDialog']]]
-];
