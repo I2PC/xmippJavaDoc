34235f0d24ab8347eb1dd4648a8e8d6cc8b9f4df
biocompwebs <biocompwebs@gmail.com>
2020-09-27 15:10:33 +0200

    buildbot automated-update

---
+++ html/search/functions_d.js
@@ -0,0 +1,7 @@
+var searchData=
+[
+  ['needstraining',['needsTraining',['../classxmipp_1_1jni_1_1Classifier.html#af09b6d7192dbd459357d76a33e2cf3f6',1,'xmipp.jni.Classifier.needsTraining()'],['../classxmipp_1_1jni_1_1PickingClassifier.html#a5dfa1a47f796e5cfeeb73ad72a61d186',1,'xmipp.jni.PickingClassifier.needsTraining()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1training_1_1model_1_1GenericClassifier.html#a139375a43f7c83afb9e1595585154d66',1,'xmipp.viewer.particlepicker.training.model.GenericClassifier.needsTraining()']]],
+  ['normalizeimageplus',['normalizeImagePlus',['../classxmipp_1_1ij_1_1commons_1_1XmippImageConverter.html#abca6fda658cf95134f39c607f5cfa879',1,'xmipp::ij::commons::XmippImageConverter']]],
+  ['numbertocolor',['numberToColor',['../classxmipp_1_1viewer_1_1particlepicker_1_1ColorHelper.html#aea9d5f253463b9547fd02af66b561ee3',1,'xmipp::viewer::particlepicker::ColorHelper']]],
+  ['numbertocolorpercentage',['numberToColorPercentage',['../classxmipp_1_1viewer_1_1particlepicker_1_1ColorHelper.html#a5d6135a4f2af90a2ee602773cfcbb38e',1,'xmipp::viewer::particlepicker::ColorHelper']]]
+];
