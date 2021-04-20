cf75b59d1508a4be79ad8889cb0f967b74134dec
biocompwebs <biocompwebs@gmail.com>
2021-04-20 19:26:04 +0200

    buildbot automated-update

--- html/search/functions_1.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['bestshift',['bestShift',['../classxmipp_1_1jni_1_1ImageGeneric.html#ae15516e6238c8c6110807abcfaa6a998',1,'xmipp::jni::ImageGeneric']]],
-  ['binarize',['binarize',['../classxmipp_1_1ij_1_1plugins_1_1maskstoolbar_1_1Operations.html#ab998cea09957042fa95913af34f6d319',1,'xmipp::ij::plugins::maskstoolbar::Operations']]],
-  ['blockgui',['blockGUI',['../classxmipp_1_1utils_1_1XmippWindowUtil.html#a95f42c31d256dd796e7410d1255021c8',1,'xmipp.utils.XmippWindowUtil.blockGUI(JFrame window, String status)'],['../classxmipp_1_1utils_1_1XmippWindowUtil.html#adc607b7a1ed79fcff20d7a01a7882c05',1,'xmipp.utils.XmippWindowUtil.blockGUI(JFrame window, String status, boolean hideContent)']]],
-  ['browsedirectory',['browseDirectory',['../classxmipp_1_1viewer_1_1particlepicker_1_1ImportParticlesJDialog.html#a7dd6cf77f3522848ee767d02767bbed9',1,'xmipp.viewer.particlepicker.ImportParticlesJDialog.browseDirectory()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1tiltpair_1_1gui_1_1ImportParticlesFromFilesTiltPairJDialog.html#a030ac4d36ce5cfc1dc528a69dd9d7acd',1,'xmipp.viewer.particlepicker.tiltpair.gui.ImportParticlesFromFilesTiltPairJDialog.browseDirectory()']]],
-  ['buildimageplus',['buildImagePlus',['../classxmipp_1_1ij_1_1commons_1_1XmippImageConverter.html#a915c4004706c4c3358a539952860a0df',1,'xmipp::ij::commons::XmippImageConverter']]]
-];
