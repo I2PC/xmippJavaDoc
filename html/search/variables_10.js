5bd36e238b96b63b4c23d0efb19c4a267f37bb51
biocompwebs <biocompwebs@gmail.com>
2021-08-16 11:55:02 +0200

    buildbot automated-update

---
+++ html/search/variables_10.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['quickhelpdlg',['quickhelpdlg',['../classxmipp_1_1viewer_1_1windows_1_1GalleryJFrame_1_1GalleryMenu.html#a27e964ecf4fe5772a8004f33ae8368a2',1,'xmipp::viewer::windows::GalleryJFrame::GalleryMenu']]]
+];
