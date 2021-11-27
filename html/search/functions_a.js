867f9f63886cccf3d0d365fa38415fd466b213cc
biocompwebs <biocompwebs@gmail.com>
2021-11-27 11:04:04 +0100

    buildbot automated-update

--- html/search/functions_a.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['keypressed',['keyPressed',['../classxmipp_1_1ij_1_1commons_1_1XmippImageCanvas.html#a59dbb880467ca51095f0fbdfe1cc054f',1,'xmipp.ij.commons.XmippImageCanvas.keyPressed()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePickerCanvas.html#aaebc1574ade5af1bbe77371a49a08b51',1,'xmipp.viewer.particlepicker.ParticlePickerCanvas.keyPressed()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1training_1_1gui_1_1SupervisedPickerCanvas.html#a2f4d386acbd2e07bc941fdd4a942df2f',1,'xmipp.viewer.particlepicker.training.gui.SupervisedPickerCanvas.keyPressed()']]],
-  ['keyreleased',['keyReleased',['../classxmipp_1_1ij_1_1commons_1_1XmippImageCanvas.html#ab7e94901ae93c8f07ba4d51aba4b6dd7',1,'xmipp::ij::commons::XmippImageCanvas']]],
-  ['keytyped',['keyTyped',['../classxmipp_1_1ij_1_1commons_1_1XmippImageCanvas.html#acfccb364f40b914f2ff3139f56dc3cdd',1,'xmipp::ij::commons::XmippImageCanvas']]]
-];
--- html/search/variables_19.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['z_5fneg',['Z_NEG',['../classxmipp_1_1jni_1_1ImageGeneric.html#a9f078a2613ddaa3411ad93966c32e68b',1,'xmipp::jni::ImageGeneric']]],
-  ['z_5fpos',['Z_POS',['../classxmipp_1_1jni_1_1ImageGeneric.html#a1bbb60b00765ad021bb0f228e1468287',1,'xmipp::jni::ImageGeneric']]],
-  ['zoom',['zoom',['../classxmipp_1_1utils_1_1Params.html#a0705efa6f264f735ff05b01d8cdc85c8',1,'xmipp.utils.Params.zoom()'],['../classxmipp_1_1viewer_1_1models_1_1GalleryData.html#a4a6a38b787a758050837a8ac80b33a4b',1,'xmipp.viewer.models.GalleryData.zoom()'],['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePicker.html#a29bb23fa101405c5689b0d1f0560659c',1,'xmipp.viewer.particlepicker.ParticlePicker.zoom()'],['../classxmipp_1_1utils_1_1Params.html#a06409dd38a2c3e1c6c259ad3ed1c8dfd',1,'xmipp.utils.Params.ZOOM()'],['../classxmipp_1_1utils_1_1XmippResource.html#ab0c4ee9e8a5a404dd3bcac6bdd16d08c',1,'xmipp.utils.XmippResource.ZOOM()']]]
-];
