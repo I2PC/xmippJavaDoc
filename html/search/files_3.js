300d8401ab7d690e904a8c86d1236c432650ddf2
biocompwebs <biocompwebs@gmail.com>
2021-12-19 18:18:17 +0100

    buildbot automated-update

--- html/search/files_3.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['debug_2ejava',['DEBUG.java',['../DEBUG_8java.html',1,'']]],
-  ['designmaskjframe_2ejava',['DesignMaskJFrame.java',['../DesignMaskJFrame_8java.html',1,'']]],
-  ['dm3_5freader_2ejava',['DM3_Reader.java',['../DM3__Reader_8java.html',1,'']]]
-];
