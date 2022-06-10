6384dc651ae82a6dd43c3505e5fd397c500bcd5b
biocompwebs <biocompwebs@gmail.com>
2022-06-10 13:32:48 +0200

    buildbot automated-update

---
+++ html/search/variables_6.js
@@ -0,0 +1,16 @@
+var searchData=
+[
+  ['gallery',['gallery',['../classxmipp_1_1viewer_1_1windows_1_1AddFillLabelsJDialog.html#a5d2d09795242fcd65f83e7b21b23523e',1,'xmipp.viewer.windows.AddFillLabelsJDialog.gallery()'],['../classxmipp_1_1viewer_1_1windows_1_1GalleryJFrame.html#a1f1dae04a5cb2306c839777b7055070e',1,'xmipp.viewer.windows.GalleryJFrame.gallery()']]],
+  ['gallery_5fmd',['GALLERY_MD',['../enumxmipp_1_1viewer_1_1models_1_1GalleryData_1_1Mode.html#a295711deb8fb5f7085a3d473961382ff',1,'xmipp::viewer::models::GalleryData::Mode']]],
+  ['gallery_5fvol',['GALLERY_VOL',['../enumxmipp_1_1viewer_1_1models_1_1GalleryData_1_1Mode.html#a88ea1608c894dd2d6693c106674a3d61',1,'xmipp::viewer::models::GalleryData::Mode']]],
+  ['gamma',['gamma',['../classlibsvm_1_1svm__parameter.html#aabbd71665d64cc45e2ed66079cce2b6b',1,'libsvm::svm_parameter']]],
+  ['gaussianblurfilter',['gaussianBlurFilter',['../classxmipp_1_1ij_1_1commons_1_1XmippImageJ.html#af63c32d61d4b93caecaf5a1cd646a630',1,'xmipp::ij::commons::XmippImageJ']]],
+  ['gbc',['gbc',['../classxmipp_1_1viewer_1_1windows_1_1AddFillLabelsJDialog.html#a5672c536f158cbd7aa9fc12cd67807b5',1,'xmipp.viewer.windows.AddFillLabelsJDialog.gbc()'],['../classxmipp_1_1viewer_1_1windows_1_1MDSearchJDialog.html#ac7e0a8a4fee5cef6b91e2a71990d216b',1,'xmipp.viewer.windows.MDSearchJDialog.gbc()'],['../classxmipp_1_1viewer_1_1windows_1_1SaveJDialog.html#a6bcbea9bb86b753bfe56524a64c8b1f3',1,'xmipp.viewer.windows.SaveJDialog.gbc()']]],
+  ['geometry',['geometry',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#ab92cdc28a6776f91a79a4500d53a2ea7',1,'xmipp::ij::commons::ImagePlusReader']]],
+  ['geometry_5flabels',['GEOMETRY_LABELS',['../classxmipp_1_1jni_1_1MetaData.html#ad2b52643c6cbfe3d230df98b4ceff2b5',1,'xmipp::jni::MetaData']]],
+  ['geometry_5frelion_5flabels',['GEOMETRY_RELION_LABELS',['../classxmipp_1_1jni_1_1MetaData.html#a3704a83cd8f317fd7e50bfa172abe7a3',1,'xmipp::jni::MetaData']]],
+  ['goto',['GOTO',['../classxmipp_1_1utils_1_1XmippResource.html#ae5e6a9dbc21283f2777d1f29e12d7bf8',1,'xmipp::utils::XmippResource']]],
+  ['gpickerpn',['gpickerpn',['../classxmipp_1_1viewer_1_1particlepicker_1_1training_1_1gui_1_1SupervisedPickerJFrame.html#a9c96456e863aa7f97b5ff0de14ee6fdf',1,'xmipp::viewer::particlepicker::training::gui::SupervisedPickerJFrame']]],
+  ['group',['group',['../classxmipp_1_1viewer_1_1windows_1_1SaveJDialog.html#a942896e63e026998a8548a78ef583cde',1,'xmipp::viewer::windows::SaveJDialog']]],
+  ['groups',['groups',['../classxmipp_1_1utils_1_1XmippMenuCreator.html#adab02f7eef5e6050c690bc245c7ce478',1,'xmipp::utils::XmippMenuCreator']]]
+];
