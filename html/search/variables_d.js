20ccf11327dc2f1791cda6c80e4a9b68b23ea495
biocompwebs <biocompwebs@gmail.com>
2022-08-19 03:45:33 +0200

    buildbot automated-update

---
+++ html/search/variables_d.js
@@ -0,0 +1,24 @@
+var searchData=
+[
+  ['n',['n',['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#ac46fa68291e1e47551ca7f9b2023dd82',1,'xmipp::viewer::models::ImageGalleryTableModel']]],
+  ['name',['name',['../classxmipp_1_1jni_1_1Classifier_1_1Parameter.html#a99f9ba18bf0c1d599d75f4899e0b87e7',1,'xmipp::jni::Classifier::Parameter']]],
+  ['newline',['newline',['../classxmipp_1_1viewer_1_1ViewerTest.html#a707268838e7fc39ea0fc9d8943e65dd2',1,'xmipp::viewer::ViewerTest']]],
+  ['next_5fmicrograph_5fkey',['NEXT_MICROGRAPH_KEY',['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePickerJFrame.html#a49e0dc1eebcfa0174fac8b7d2fe0f10e',1,'xmipp::viewer::particlepicker::ParticlePickerJFrame']]],
+  ['nextcolor',['nextcolor',['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePicker.html#af42e955e3f05cb2028ad9e038b9a5ed0',1,'xmipp::viewer::particlepicker::ParticlePicker']]],
+  ['no_5fgeo',['NO_GEO',['../classxmipp_1_1utils_1_1Params.html#ac5d196e11ce8d43ad51dcd89c1002e85',1,'xmipp::utils::Params']]],
+  ['no_5foption',['NO_OPTION',['../classxmipp_1_1utils_1_1XmippQuestionDialog.html#a06bd62183a77db22f44ebfdca7b30b05',1,'xmipp::utils::XmippQuestionDialog']]],
+  ['no_5fwrap',['NO_WRAP',['../classxmipp_1_1utils_1_1Params.html#a91f0b8a047659b16e198847597da7c22',1,'xmipp::utils::Params']]],
+  ['none',['None',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#adb05937c5f3cf9021f520e6565aef179',1,'xmipp::viewer::particlepicker::Format']]],
+  ['normalize',['normalize',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#aaff95c63f90e40971050a680f72490c5',1,'xmipp.ij.commons.ImagePlusReader.normalize()'],['../classxmipp_1_1viewer_1_1models_1_1GalleryData.html#ac302d722cca2d8082bcefd382711ce00',1,'xmipp.viewer.models.GalleryData.normalize()'],['../classxmipp_1_1utils_1_1XmippResource.html#ab233aadb9f2fbbb5c78d0966d3f5f86a',1,'xmipp.utils.XmippResource.NORMALIZE()']]],
+  ['normalize_5fcalculated',['normalize_calculated',['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#a352e7ef10b5557b8d9646d3ccf6ae6f9',1,'xmipp::viewer::models::ImageGalleryTableModel']]],
+  ['normalize_5fmax',['normalize_max',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#a798c9a5af25307728dbacbae2f34767b',1,'xmipp::ij::commons::ImagePlusReader']]],
+  ['normalize_5fmin',['normalize_min',['../classxmipp_1_1ij_1_1commons_1_1ImagePlusReader.html#acadfe9a7f88439164a28a509a8c77c96',1,'xmipp.ij.commons.ImagePlusReader.normalize_min()'],['../classxmipp_1_1viewer_1_1models_1_1ImageGalleryTableModel.html#a54c15293fa69f680793d4ce3eadd8aa4',1,'xmipp.viewer.models.ImageGalleryTableModel.normalize_min()']]],
+  ['nr_5fclass',['nr_class',['../classlibsvm_1_1svm__model.html#a584eac9f4e87ac8ed5c31259a2d83165',1,'libsvm::svm_model']]],
+  ['nr_5fweight',['nr_weight',['../classlibsvm_1_1svm__parameter.html#a2a36d1565cbc0106f87f0c9abed6c3fa',1,'libsvm::svm_parameter']]],
+  ['nsv',['nSV',['../classlibsvm_1_1svm__model.html#af0bc41570bb4d89340ef9486491ef896',1,'libsvm::svm_model']]],
+  ['nu',['nu',['../classlibsvm_1_1svm__parameter.html#a373b74244ae284fc4a6db91f1200902d',1,'libsvm::svm_parameter']]],
+  ['nu_5fsvc',['NU_SVC',['../classlibsvm_1_1svm__parameter.html#a46b246f723ecbbc6571a447f14f7cac1',1,'libsvm::svm_parameter']]],
+  ['nu_5fsvr',['NU_SVR',['../classlibsvm_1_1svm__parameter.html#a9bb0974571faa5a87be09a9b05bb8f3a',1,'libsvm::svm_parameter']]],
+  ['numberofclasses',['numberOfClasses',['../classxmipp_1_1viewer_1_1models_1_1ClassInfo.html#af61b336adcddc7c8324962b0a7924de3',1,'xmipp::viewer::models::ClassInfo']]],
+  ['numberofimages',['numberOfImages',['../classxmipp_1_1viewer_1_1models_1_1ClassInfo.html#a08cebe814ac9e9e13187f7e1e11f7f36',1,'xmipp::viewer::models::ClassInfo']]]
+];
