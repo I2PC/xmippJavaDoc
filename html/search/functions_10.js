75b33fc9e2a0ab95f227a75681e695078cd630a6
biocompwebs <biocompwebs@gmail.com>
2022-10-04 05:46:54 +0200

    buildbot automated-update

--- html/search/functions_10.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['quickhelpjdialog',['QuickHelpJDialog',['../classxmipp_1_1utils_1_1QuickHelpJDialog.html#aa2551572aaab82980762013f3c5c3b43',1,'xmipp.utils.QuickHelpJDialog.QuickHelpJDialog(Frame window, boolean modal, String helptitle, Map&lt; Object, Object &gt; map)'],['../classxmipp_1_1utils_1_1QuickHelpJDialog.html#a185489ec63eec81d19d4b2eb77cb77da',1,'xmipp.utils.QuickHelpJDialog.QuickHelpJDialog(Frame window, boolean modal, String helptitle, Map&lt; Object, Object &gt; map, boolean editmap)']]],
-  ['quickhelppane',['QuickHelpPane',['../classxmipp_1_1utils_1_1QuickHelpPane.html#a7fcb3f5e125319a6214288c5f5e123b0',1,'xmipp.utils.QuickHelpPane.QuickHelpPane(String title, Map&lt; Object, Object &gt; helpmap)'],['../classxmipp_1_1utils_1_1QuickHelpPane.html#a62c1e5061f70dd287015590da992f64e',1,'xmipp.utils.QuickHelpPane.QuickHelpPane(String title, Map&lt; Object, Object &gt; helpmap, boolean editmap)']]]
-];
