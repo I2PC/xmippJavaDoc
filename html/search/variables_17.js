08379d7b21a87f18c509397aeda07821d286b7a6
biocompwebs <biocompwebs@gmail.com>
2023-11-28 16:35:22 +0100

    buildbot automated-update

---
+++ html/search/variables_17.js
@@ -0,0 +1,15 @@
+var searchData=
+[
+  ['x',['x',['../classlibsvm_1_1svm__problem.html#ab551d9238bf36c4ba83c018569b09fbb',1,'libsvm.svm_problem.x()'],['../classxmipp_1_1jni_1_1Particle.html#ac0b72ca4df6b0c4cf06eda181b2b921c',1,'xmipp.jni.Particle.x()']]],
+  ['x_5faxis',['X_AXIS',['../enumxmipp_1_1utils_1_1CompoundIcon_1_1Axis.html#a6bb0b04c44bc5cb49865f12a26ad723e',1,'xmipp::utils::CompoundIcon::Axis']]],
+  ['x_5fneg',['X_NEG',['../classxmipp_1_1jni_1_1ImageGeneric.html#ab02f9f26c4baae5f982c4a66d5c37afb',1,'xmipp::jni::ImageGeneric']]],
+  ['x_5fpos',['X_POS',['../classxmipp_1_1jni_1_1ImageGeneric.html#a2a45dc224a065b8d4f8c97cde0f21640',1,'xmipp::jni::ImageGeneric']]],
+  ['xfc',['xfc',['../classxmipp_1_1viewer_1_1particlepicker_1_1ImportParticlesJDialog.html#ac96e1d55afba74f3ec1f728b638fcb6c',1,'xmipp::viewer::particlepicker::ImportParticlesJDialog']]],
+  ['xmipp',['Xmipp',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#af1cc57c5a932c0c06432a8d5707e452f',1,'xmipp::viewer::particlepicker::Format']]],
+  ['xmipp24',['Xmipp24',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#ab9fb892652f17bc876e44661130c2726',1,'xmipp::viewer::particlepicker::Format']]],
+  ['xmipp30',['Xmipp30',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#af143f519669243a5fb886d2ff192fa09',1,'xmipp::viewer::particlepicker::Format']]],
+  ['xmipp301',['Xmipp301',['../enumxmipp_1_1viewer_1_1particlepicker_1_1Format.html#a5cacf97d6a2b87cba9e0a04b1c5d3b5e',1,'xmipp::viewer::particlepicker::Format']]],
+  ['xmipp_5fsupported',['XMIPP_SUPPORTED',['../classxmipp_1_1jni_1_1Filename.html#a3dc5d070cf11d19c03e11419f66979ca',1,'xmipp::jni::Filename']]],
+  ['xmippsmoothfilter',['xmippsmoothfilter',['../classxmipp_1_1viewer_1_1particlepicker_1_1ParticlePicker.html#ae29f4a6ff694ad4825e1f79633e2cd8f',1,'xmipp::viewer::particlepicker::ParticlePicker']]],
+  ['xw',['xw',['../classxmipp_1_1ij_1_1commons_1_1XmippMenuBar.html#a488f6602f6bb8adbff45ada2ecf69abe',1,'xmipp::ij::commons::XmippMenuBar']]]
+];
